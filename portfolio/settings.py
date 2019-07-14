75c88beedec2cebc4b2d4eab794f46aeb2fa350f
Pirunpoika <kirkkonen@gmail.com>
2019-07-14 15:50:11 +0300

    Site polished

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -120,10 +120,13 @@ USE_L10N = True
 USE_TZ = True
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 MEDIA_URL = '/media/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'portfolio/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
